4299ad4347d56abfbc940ce2bf8dcdb62b334db2
Yashvant Kumar <87422165+GYashvant@users.noreply.github.com>
2021-11-30 19:28:34 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,9 @@ const connect = require('./config/db')
 const app = require('./index')
 // app.use(express.json())
 
-app.listen(2000, async function (){
+require("dotenv").config()
+
+app.listen(process.env.PORT || 2000, async function (){
     await connect();
 console.log("listening on port 2000");
 });
