8db81e07134a4f3434dcfc8551d5d4b2318dc180
Yashvant Kumar <87422165+GYashvant@users.noreply.github.com>
2021-12-03 10:18:59 +0530

    Update News.jsx

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -61,7 +61,7 @@ function News(){
     }, [])
 
     const getNews = async() => {
-      let res = await fetch('http://localhost:2000/news')
+      let res = await fetch('https://mysterious-spire-55595.herokuapp.com/news')
         let newsData =  await res.json()
         setNewsArticles(newsData)
     }
