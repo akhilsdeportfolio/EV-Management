ae794e1dcafaf9811f1006cf7c804c5a98fde459
Deepak Chaurasiya <dchaurasiya8589@gmail.com>
2021-12-03 11:22:09 +0530

    Improved Gallery Page

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,10 +1,8 @@
 import { Footer } from "./Footer";
 import * as React from 'react';
 import Box from '@mui/material/Box';
-
 import Modal from '@mui/material/Modal';
-import { blue } from "@mui/material/colors";
-import { Navbar } from "./Navbar";
+import {Navbar} from "./Navbar"
 let styles = {
     style:{
         position: 'absolute',
@@ -17,8 +15,8 @@ let styles = {
         border: '2px solid #000',
         boxShadow: 24,
         p: 4,
-      },
-      lines:{
+    },
+    lines:{
         width:10,
         height:65,
         background:"#FF546D",
@@ -35,14 +33,14 @@ let styles = {
         lineHeight: "49px",
         color: "#000000", 
     },
-      tt:{
+    tt:{
         width:100,
-    
-      },
-      zoomIn:{
-          width:"100%",
-          height:"100%"
-      },
+
+    },
+    zoomIn:{
+        width:"100%",
+        height:"100%"
+    },
     contain:{
         width: "90%",
         marginLeft: "5%",
@@ -73,34 +71,30 @@ let styles = {
         height: "237px",
         cursor:"pointer",
     },
-    
     white:{
         position:"absolute",
-        top:"236%",
+        top:"268%",
         borderRadius:10,
         width: "29.2%",
         height: "10px",
-       
         backgroundColor:"#5D6D61",
-       
         marginRight:"1.7%",
         float:"left"
     },
     grey:{
         position:"absolute",
-        top:"236%",
+        top:"268%",
         borderRadius:10,
         left:"35.6%",
         width: "29%",
         height: "10px",
         marginRight:"1.7%",
-       
         backgroundColor: "#FF546D",
         float:"left"
     },
     blue:{
         position:"absolute",
-        top:"280%",
+        top:"268%",
         borderRadius:10,
         width: "28.4%",
         height: "10px",
@@ -114,30 +108,30 @@ let styles = {
     },
     playButtonImg:{
         position: "relative",
-    top: "-170px",
-    left: "210px",
-    width: "40px",
-    height: "40px"
+        top: "-170px",
+        left: "210px",
+        width: "40px",
+        height: "40px"
     }  
 }
+
 export const Gallery = ()=>{
-    const [pic,setPic] = React.useState("xyz")
-    const [open, setOpen] = React.useState(false);
-     const handleOpen = ((e)=>{
+        const [pic,setPic] = React.useState("xyz")
+        const [open, setOpen] = React.useState(false);
+        const handleOpen = ((e)=>{
         setPic(e.target.src);
-         setOpen(true);
-        return
-    })
-    const handleClose = () => setOpen(false);
-    function ImageList(props){
+            setOpen(true);
+            return
+        })
+        const handleClose = () => setOpen(false);
+        function ImageList(props){
         const images = props.image;
         const listItems = images.map((el)=>
             <img style={styles.img} src = {el} alt="" onClick = {handleOpen}/>
         );
         return (
             <div style={styles.row1Img}>{listItems}</div>
-        )
-    }
+        )}
     function fun1(){
         setVisible3(false)
         setVisible2(false)
@@ -165,22 +159,22 @@ export const Gallery = ()=>{
             setColor4(whiteRow4)
             setColor7(whiteRow7)
         }
-        else if(el.target.src ==="https://s3-alpha-sig.figma.com/img/1ed9/c597/d8de9e461aa8cc6700ff9fe8ba548e15?Expires=1637539200&Signature=bpqukmDRnptOkkZI8qsNDcb~m23U3KqwmLCc5UlMRAQ~x2CGOaCwQHzrm7GIBY-cZaVqR9dbdeh2tx52IoGT2fshig0fzuUhRKkpfeWrNIJCcrgy1mEQns2hfYbYH3NCdLInB9KIArUlfDTksaudMMqYcvdevFWDOTlRIi0Hb8x1SjbnfigTUs~ACzNiltA7XwETe2Rj7Fo3keqggu6nwsD8mnGf0GyUX3bnUR3aJuQGMOUhun6XtkcJxlh6dF7N8p0t4-aW7-qidbVw5HgF3gbqLbaeDkVfx9ZJeCuKZa4EzaAHKlWCi~HI8~1Mxhk9FN8sccPA9Zmp0Qe8N45FTg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"){
-           fun2()
-           setColorLine({...styles.line,background:"rgb(3, 128, 190)"})
-           setColorLine2({...styles.lines,background:"rgb(3, 128, 190)"})
-           setColor6(row1Img)
-           setColor(row2Img)
-           setColor2(row3Img)
-           setColor3(row4Img)
-           setColor4(row6Img)
+        else if(el.target.src ==="https://s3-alpha-sig.figma.com/img/9026/ff35/a00dd10fd914ecd93f0abe139b563f5b?Expires=1637539200&Signature=Q0DssljF4d4AgB2sn8OUX2jLiQCytmW0k6xe0367Vn4smryzSuoRg9VMbC8Zv-~EIzcau8VJ1OavqUaxG15sjmRaqavI4lptzN1TbBYxP092MWb1VbDppnMrGt5cWKA7kYoF-XtlNJMOyJZGvQlGgBfU~GThog23AvrFVwrvgcJEnfO9jR3lYK9iNuxaUGMfU-k37CNrGyukCH~cI0I6zq6PHlu~gABZfk6~LjjFSxh1ryD~vlSuZOH~yjru1p1iaiswQCOrCPjDiOBEAHz-6DC5GmbvBGkJXFcmGeHYGMnmGo3ebyDMn6vrq76NSHChHzYmdhE006mQZ2a8QSKaaA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"){
+            fun2()
+            setColorLine({...styles.line,background:"rgb(3, 128, 190)"})
+            setColorLine2({...styles.lines,background:"rgb(3, 128, 190)"})
+            setColor6(row1Img)
+            setColor(row2Img)
+            setColor2(row3Img)
+            setColor3(row4Img)
+            setColor4(row6Img)
             setColor7(row7Img)
         }
         else if(el.target.src ==="https://imgd.aeplcdn.com/1056x594/n/cw/ec/26918/xuv300-exterior-right-front-three-quarter-148706.jpeg?q=85&wm=1"){
             fun3()
             setColorLine({...styles.line,background:"rgb(255, 84, 109)"})
             setColorLine2({...styles.lines,background:"rgb(255, 84, 109)"})
-        setColor6(redRow6)   
+            setColor6(redRow6)   
             setColor(redRow1)
             setColor2(redRow2)
             setColor3(redRow3)
@@ -190,9 +184,7 @@ export const Gallery = ()=>{
     }
     function ColorList(props){
         const images = props.image;
-        
         const listItems = images.map((el)=>
-        
             <img style={styles.img} src = {el} alt="" onClick = {colorChange} />
         );
         return (
@@ -202,7 +194,6 @@ export const Gallery = ()=>{
 
     function VideoList(props){
         const images = props.image;
-        
         const listItems = images.map((el)=>
             <iframe  width="95%" height="245" src={`https://www.youtube.com/embed/${el}?autoplay=1&mute=1`} title = "Nexon" onClick = {handleOpen} >
             </iframe>
@@ -216,7 +207,7 @@ export const Gallery = ()=>{
     var row2Img = ["https://s3-alpha-sig.figma.com/img/208b/a13c/0d834497bd602af2168a48df4f92b960?Expires=1637539200&Signature=XBUB6eWQLZphdiHWqwxSjeFlEUUAyqe4q-3q~IhHLciJAHbW26LFsiQ2rUD3JOIWNYC31nVDwYi9XnvU2Dhdhh2kbalm~9K3T93aTyvZnsvPv~f9pe1lWfBvOHWjYzgC21QFgGiVfQ93-yPGGXOLdj-aN4iBdhgvHDAGIcskZ4Awhl6DZPavxKz6EbbtPmCw85XofcJhUsPR~sKsfomjQReOQhbQKdF2TGrVQEdwjtlynFhx2G66mT5mxoryH42MsRaA3YhZFBZLq7n-B6xxkV1CSdd0drdLuQF52HKIcQc38wUXV4asYKdxipWFxc5NTx4LPmpETjxC0i1avkayog__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA","https://s3-alpha-sig.figma.com/img/59bd/6810/537cabb71195af1126883c6cbb0ad682?Expires=1637539200&Signature=a1eM~rwY0sSLSTmzWrccqzVUsiIb2rnt2fKTOGgiNm0YCheQZNsMFjhY6PpEcRAF3uZ0V9M8ntB4hD0Qm1nGl5DuglG3asy4SeI5Lx0HlTtU8TI-YemaZriPhj8BJzHrOVJ~9IiyNBtTD-vrNVLHv1m1obWcAVwPXVQlWY7q1z~oBKxdmvOcwb3xTdZbDNVT2AYUpnUCprsRpdCPaoCorekLb1DY4u-Rf~RA~scw8~dXrZiTsz0tFXtJHrISsudEQsQsKAm8JHyLJTuHEB2BWvgHVF7vjG5SBFNgjCXm~daitvnnV9eyQcE989PTFNahuvbDEE~8PsgpKYrwP~hx4Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA","https://s3-alpha-sig.figma.com/img/2c34/e222/9440bf9445bb92d3596513809a8ce731?Expires=1637539200&Signature=MXFK79zKOUMjqCUxFLpWMT-z-t06kxgC6vFyJj6b5MTEj6q6z9aFFISrbQ4kJMOChhQXzGmyeLDIUFcCT9TvLFBjVNWKA1qXoyCIfTBUZfHBMwJGmHBeYWP1FR-jq5L4fzVJIlNUirtEW0SgHpYMWgRJpXgfcD294RdtTZzLsxTd34tQT-j7pGnjkfPup6D7umt4dnJ29-MuHpfMXurtKzcDRbCvKSnA6P1Nc2eaNNhhQSwXgWECMo0pZMQqAsSNV0mmPU0NGB0rS3f9TAlg8IkxG2ebJlcc1TTJO~dEU13jl89rY4DZOyzWY8STBtLNFgjwN3oPgeT50z1b0nqjAg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"]    
     var row3Img = ["https://s3-alpha-sig.figma.com/img/0120/c959/d530f686e4c9af93f2c1d910a60acdc4?Expires=1637539200&Signature=bJbj68WS0aRmPNwvWiuGugDYB4Ti99Eakf4-INF-pAP22ygcdpIjZ~doHlzdiRm2usHU804ZqqtEkc5Oq9GdErylQmkqP1eo2ArScyiYpYK38tZFSBmyGJupPFaXYQV79QYHzZl293WCiC~sNBMBUyrHzlF7D7xTt5uKax5AWrEQHFJrjD9ojS02-UqjbQWj-ocfyK8JiRB5semoJymgj47jl6izz9jW2sqo5ZOYUItCjhSBPkWcu0R6cRcxpH3X590yhxmcM0d5E2H3Y3zdgtAooVbPlgaJk2rz75x5lHEsGWck3kwxZTKs3LxCiqIJIJK5WwBVMdJoVe3zhGy3WA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA","https://s3-alpha-sig.figma.com/img/63d3/0872/c329a0a1ec5fca783f1527e4efba6e61?Expires=1637539200&Signature=h5FaTl7P5pb6UMfz9lRxwiaYROy02KjeBlyymeeeK7W-UQ4LGHwLcdbOSh3nI~dCVF6jRjHfODfTIpW619Pe3roTm3CrYJQcGp3OTDCc3qrUDDLPuTHxs9IGsMUksIXqP0u95gCjB6zFxccd~~awrRdjDjWWAK0bDRVpbv6D6lDz87zMnqs6InS3cnV1DY6S4V66hmZ70C3RRwFd01kx4Xt3phsLV7W9KZ52jVIv2-tRiwHXIu6G-C~jOKwwdlzMwKJJm4cUrHKK~FSHJnVQ7LliXiOYbQnx8uxzFewiTbfBE7VbJ8T28~mMEiph8tMvXz-mFsv1gukanpUCSGvaiw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA","https://s3-alpha-sig.figma.com/img/c237/3ab5/53d94a7106076b23e52aeb9a8a239bfb?Expires=1637539200&Signature=O14W9RvVi470wieGevpZJvvWAUSbiGvuaIH5BBFn9GM04WVVzRPo9APBa7DKFpia5Evr~6Nqz0ruq5ibq0f1J1wCvnDPD--s4JDgpMP3a82O7aedviQfdSN39nTVwXWv68LwI5EIgTT~IhFbdqpv4wZTUIMGE38QbiNxRDMVuJ~vYAmyann1WDuROiLvgFl-SeNnLQkYqbKFTSmGB~a6V6xtJFfnBNS~-UZ7ng7WpR3f3HucYjyZ2KDlwq4vpfm9hhjEkpBTntkhuzTrirma~DsrGxTKaBgC5kdrnoSp6EiRJB5ckQd6IIIF23~0HQ6DUpKE47k7y7EVHUAzBk-E9A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"]
     var row4Img = ["https://s3-alpha-sig.figma.com/img/7c94/092a/c8663a64c3d79b102290ef6bbdd8b681?Expires=1637539200&Signature=eqMRif5ngHwXCKKaPIAevQVcRQaMdWuzNTgIiJv~TMHkXHTHratVNs3vk0Wy0rbnDk3r14Rv~AwfmPmNc37RK2QU8nH88b7ZtHPaATr6BkvbpmO1HqolLqoG~WhoRu4tT2pZVQiHP7UkhmPxrVAv5B-LjI7oZvPspOr21fXzge7KZxU4pNmu3K5~kX60Zsvm-KaG7ivLUxFGLzzyL~e1I4HcUTtdxOcdgcQAW9Xbw-OnFGQUYOU-IsfJQHkrZL7U6cUI5LKMiwr3dHksE~plOvjMzaueUIuCETGL-wnzV4OF63DRKoQnYQ3V7dsi0F0Ok17boMdbUSiciAt9ziIfZw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA","https://s3-alpha-sig.figma.com/img/c746/af80/6f53a6f3028cbbb747c885c6a6a97a29?Expires=1637539200&Signature=EaVUUuIV5zlbq5qR8ynpcQ6qglt12GplPQw1iJ7Zfg1wmENM6gIhWV7sMaHJ9t9G2JkgEvIOG~zNchD6OAHXMnCakn9a-GvUuU3e0TR4AoaAVHandt6p7~qLork7Wg3hsd5md35glCiJ12UZe5ThH4VPb92~p3U-uea6X6rgiF8dZ6oy9810AcaqC555byXAsZnmKJcSvBoO6fS84yJiWojabLqZVF~GMlGDJrsxZ4xKPf6B-lGWz2zR~SZrRTwb3Zhlw1azTQvOz~ODKAuQWiutysdEHoyi7d-2hSixBL8a5J3yJzfIMf0azAmZQPnJ7~bz~bDFiFhgZZAdIwisXg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA","https://s3-alpha-sig.figma.com/img/e7a2/9d3c/f547fc1edb1015692bb19621070f6b49?Expires=1637539200&Signature=hYe8orViGtQXex1vec2VRs9kZX2M9lOrrjbCQksvZiE1gauzL7tCIHQBEEeM6ajd3utSC7F~2bXYsQEMbynoHxMpkiN6O4EdVbjXqM9EoG4DvoACMXQ9WhXIR6KQ0UZQW-9O4lFKbea10PT0LWWhSWhz3n7gcITRzy0k0b6CqFpklwZP~3WKwLzvDk0rnv35NXOp1ISurfBl8PTTNcX5RkYFTa3wiId4VlLG-PL7110jP3cRIy8nsWK3SLKXleHGL4CkylDLWUSLlTBT0g7AuzFMdfi4WJraGqlNi8HNz3d~iH-rEppQrHZ6ALYaXhInliyC7A~gYcz1kKydOqENyg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"]
-    var row5Img = ["https://imgd.aeplcdn.com/664x374/n/cw/ec/41645/tata-nexon-right-front-three-quarter3.jpeg?q=85","https://imgd.aeplcdn.com/1056x594/n/cw/ec/26918/xuv300-exterior-right-front-three-quarter-148706.jpeg?q=85&wm=1","https://s3-alpha-sig.figma.com/img/1ed9/c597/d8de9e461aa8cc6700ff9fe8ba548e15?Expires=1637539200&Signature=bpqukmDRnptOkkZI8qsNDcb~m23U3KqwmLCc5UlMRAQ~x2CGOaCwQHzrm7GIBY-cZaVqR9dbdeh2tx52IoGT2fshig0fzuUhRKkpfeWrNIJCcrgy1mEQns2hfYbYH3NCdLInB9KIArUlfDTksaudMMqYcvdevFWDOTlRIi0Hb8x1SjbnfigTUs~ACzNiltA7XwETe2Rj7Fo3keqggu6nwsD8mnGf0GyUX3bnUR3aJuQGMOUhun6XtkcJxlh6dF7N8p0t4-aW7-qidbVw5HgF3gbqLbaeDkVfx9ZJeCuKZa4EzaAHKlWCi~HI8~1Mxhk9FN8sccPA9Zmp0Qe8N45FTg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"]
+    var row5Img = ["https://imgd.aeplcdn.com/664x374/n/cw/ec/41645/tata-nexon-right-front-three-quarter3.jpeg?q=85","https://imgd.aeplcdn.com/1056x594/n/cw/ec/26918/xuv300-exterior-right-front-three-quarter-148706.jpeg?q=85&wm=1","https://s3-alpha-sig.figma.com/img/9026/ff35/a00dd10fd914ecd93f0abe139b563f5b?Expires=1637539200&Signature=Q0DssljF4d4AgB2sn8OUX2jLiQCytmW0k6xe0367Vn4smryzSuoRg9VMbC8Zv-~EIzcau8VJ1OavqUaxG15sjmRaqavI4lptzN1TbBYxP092MWb1VbDppnMrGt5cWKA7kYoF-XtlNJMOyJZGvQlGgBfU~GThog23AvrFVwrvgcJEnfO9jR3lYK9iNuxaUGMfU-k37CNrGyukCH~cI0I6zq6PHlu~gABZfk6~LjjFSxh1ryD~vlSuZOH~yjru1p1iaiswQCOrCPjDiOBEAHz-6DC5GmbvBGkJXFcmGeHYGMnmGo3ebyDMn6vrq76NSHChHzYmdhE006mQZ2a8QSKaaA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"]
     var row6Img = ["https://s3-alpha-sig.figma.com/img/89c5/d575/0c19acbe2d425e7f7808c6f7fd43efca?Expires=1637539200&Signature=SvMv4ksio9B0u-Q9XGkONv71s4L6swobyVpseN3D-uPVjODEyWpIHvXt0fjAmUYhTTSJBVdqyXAff~Mvb3PC8FSEm2ZWHLUj88M9SPXjsuD~75Fl8dKM2eFdoBqBFYfuKrfJXDeZVnX5cmVg36sJc0ixaY2oIDIX2unQA6s-0Olh4uwlbpc-TGbEFNWz2DHqMEba96x8Hp47X6bELTVG~k9h2ubhhjQWOiwG75cabPshNSJl4aJqh9xm1eekTuOQBqw1grofHR0T4q7cgJaGDazACcRGr3HWoskIrU15AnNvFDpqfk6Uv8mIAAAWeUSwO0Ei3E5RK4qLc8XKTUkdzQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA","https://s3-alpha-sig.figma.com/img/1096/b530/d0751bba10bbd04efb905f64162014b5?Expires=1637539200&Signature=WCuNLju3ynEX56fCXWVNs8qPjSgEA5gK56XKs~Ee3vWtnfXom7Qt33X6E0TNqk5qYw4E4f7wgaax3508pdSfLlpqUpSz-miv2R4GERe7ALdDo1g~qO5ACy38Bg4lLcWk39KWnSKjgnYRCQp8TN80heJaZjpbLxdxWl2eJCrdG5jvmGXw0w0Be9RNwjzx4~uPqIkpGHiQAUP7VyqR4Ya2fNoUfFciI1-X92C0OSNQwsY3mhVyXilBK9ZKkht4Q0h3Ld~3lVQKgJuKObK40sV7NH~1OgvuKF5HznygiQk-ptE9VuY6Nf~DA2wMJgwTDxEo3~ytDzva7Ne61WiyA6HU7g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA","https://s3-alpha-sig.figma.com/img/38ee/3f9d/4ef801e3c15edf70099e99c1bc65ec60?Expires=1637539200&Signature=JVdgbc~4lLKW~NWK5KS7OgwngHcNQEpJBL0KOGk2Jnut-~OdrMi2u6RoCG4BGj3TQ717RwcX~VQTaNKSKxO4MbM0NSvV7ormldFeaijgll-fjAhggt6fYVUSPDBMEY8CFCou72z3nWRcCrDv~Sb6Nnl66ceCcAAtnH4E1kipcenmwrIo2e7o6jCT-98BhZuYiGlKtKUCdnb9Q3xS-nn8BYYmpO-xC3JWBrK1hu5EqVtEWHpWTy4rBsSGfe1YOCp6eXIP0Gm2XI5rKqoe9LtFlWnJk8oaigEdAVwCuYI1sT5scwAfwz7J6lG77AbdwDicLrQ7fuHp~1F3u8Lvn53WNQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"]
     var row7Img = ["72UlK3A2lAw","JsTY1jGOvO0","-XAZ8wgqik4"]
     
@@ -227,6 +218,7 @@ export const Gallery = ()=>{
     const whiteRow4 = ["https://stimg.cardekho.com/images/carinteriorimages/930x620/Tata/Nexon/7988/1617771639398/rear-view-mirror-courtesy-lamps-64.jpg","https://stimg.cardekho.com/images/carinteriorimages/930x620/Tata/Nexon/8497/Tata-Nexon-XZA-Plus-Dark-Edition/1625825579881/rear-seats-52.jpg","https://stimg.cardekho.com/images/carinteriorimages/930x620/Tata/Nexon/7988/1617771639398/knob-selector-184.jpg"]
     const whiteRow6 = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRqxD2NHW3KlgwhSvcqc-J9Vqk1Swr90Sklg&usqp=CAU","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVsjY4jlb7PVczQoNJTKzgnrWP7CsMHFJz_g&usqp=CAU","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMtj_uqVQToU11HHjBVUBJ2tQpgZMLpfflMA&usqp=CAU"]
     const whiteRow7 = ["3E_EAGt_RJU","zr7VE0FnwQY","tHNMWvW3BR8"]
+    
     const [color,setColor] = React.useState(row1Img)
     const [color2,setColor2] = React.useState(row2Img)
     const [color3,setColor3] = React.useState(row3Img)
@@ -250,54 +242,54 @@ export const Gallery = ()=>{
     return (
         <>
            <Navbar/> 
-           <div style={styles.contain}>
-                        <div style={styles.gallery}>Gallery</div>
+                <div style={styles.contain}>
+                    <div style={styles.gallery}>Gallery</div>
                         <div style={colorLine}></div>
-                        <div style = {
-                            colorLine2
-                        }></div>
-                        <div style={styles.exterior}>Exterior</div>
-                        <ImageList image = {color}/>
-                        <ImageList image = {color2}/>
-                        <div style = {
-                        colorLine2
-                        }></div>
+                            <div style = {
+                                colorLine2
+                                    }></div>
+                                        <div style={styles.exterior}>Exterior</div>
+                                            <ImageList image = {color}/>
+                                        <ImageList image = {color2}/>
+                                    <div style = {
+                                colorLine2
+                            }></div>
                         <div style={styles.exterior}>Interior</div>
-                        <ImageList image = {color3}/>
-                        <ImageList image = {color4}/>
-                    <Modal
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                    >
-                        <Box sx={styles.style}>
-                            <img src={pic} alt="" style = {styles.zoomIn} />
-                        </Box>
-                    </Modal>
-                    <div style = {
-                        colorLine2
-                    }></div>
-                    <div style={styles.exterior}>Colors</div>
-                    <ColorList image = {row5Img}/>
-                    <div style = {styles.selectColor}>
-                        {visible && <p style = {styles.white}></p>}
-                        {visible2 &&  <p style = {styles.grey}></p>}
-                        {visible3 && <p style = {styles.blue}></p>}
-                    </div>
-                    <div style = {
-                        colorLine2
-                    }></div>
-                    <div style={styles.exterior}>Road Test</div>
-                    <ImageList image = {color6}/>
-                    <div style = {
-                        colorLine2
-                    }></div>
-                    <div style={styles.exterior}>Videos</div>
-                    <VideoList image = {color7}/>
-                    <div style = {styles.playButton}></div>
-                </div>
-            <Footer/>
+                    <ImageList image = {color3}/>
+                <ImageList image = {color4}/>
+            <Modal
+        open={open}
+    onClose={handleClose}
+aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+        >
+            <Box sx={styles.style}>
+                <img src={pic} alt="" style = {styles.zoomIn} />
+                    </Box>
+                        </Modal>
+                            <div style = {
+                                colorLine2
+                                    }></div>
+                                <div style={styles.exterior}>Colors</div>
+                            <ColorList image = {row5Img}/>
+                        <div style = {styles.selectColor}>
+                    {visible && <p style = {styles.white}></p>}
+                {visible2 &&  <p style = {styles.grey}></p>}
+            {visible3 && <p style = {styles.blue}></p>}
+        </div>
+    <div style = {
+colorLine2
+    }></div>
+        <div style={styles.exterior}>Road Test</div>
+            <ImageList image = {color6}/>
+                <div style = {
+                    colorLine2
+                        }></div>
+                <div style={styles.exterior}>Videos</div>
+            <VideoList image = {color7}/>
+         <div style = {styles.playButton}></div>
+    </div>
+<Footer/>
         </>
     )
 }
